dfd4c74980122c9857b5ac6cd4845f1e3c5ffd8b
Aleksi Johansson <aleksi.johansson@wunder.io>
2018-07-06 18:37:46 +0300

    Back to jQuery with proper dependencies.

--- js/client_ip_fetcher.js
+++ js/client_ip_fetcher.js
@@ -1,21 +1,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.clientIpFetcher = {
     attach: function () {
-      // Request the client IP.
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', '/ip_anonymisation_helper/client_ip');
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          var data = JSON.parse(xhr.responseText);
-          if (typeof drupalSettings == 'undefined') {
-            var drupalSettings = {};
-          }
-          drupalSettings.ip_anonymisation_helper = {
-            client_ip: data['client_ip']
-          };
-        }
-      };
-      xhr.send();
+      $.getJSON('/ip_anonymisation_helper/client_ip', function(data) {
+        drupalSettings.ip_anonymisation_helper = {
+          client_ip: data['client_ip']
+        };
+      });
     }
   }
 })(jQuery, Drupal);
